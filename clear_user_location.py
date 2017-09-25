63b2b71c81ca1f0f99f96b385fdfa134d7c11b78
Jolly23 <pilot_lei@foxmail.com>
2017-09-25 17:35:11 +0800

    fix：dev：调整代码格式

--- clear_user_location.py
+++ clear_user_location.py
@@ -7,18 +7,15 @@ if __name__ == '__main__':
 
     conn_l = connect_db(PG_DATABASE_LOCALHOST)
     cursor_l = conn_l.cursor()
-    cursor_l.execute(
-        "SELECT OPENID FROM USER_LOCATION GROUP BY OPENID ORDER BY COUNT(OPENID) DESC"
-    )
+    cursor_l.execute("SELECT OPENID FROM USER_LOCATION GROUP BY OPENID ORDER BY COUNT(OPENID) DESC")
     openid_list = [x[0] for x in cursor_l.fetchall()]
     count_all = len(openid_list)
     counter = 0
     counter_all_delete = 0
     for each_openid in openid_list:
         counter += 1
-        cursor_l.execute(
-            "SELECT ID, RECORD_TIME FROM USER_LOCATION WHERE OPENID = %s ORDER BY RECORD_TIME", (each_openid,)
-        )
+        cursor_l.execute("SELECT ID, RECORD_TIME FROM USER_LOCATION WHERE OPENID = %s ORDER BY RECORD_TIME",
+                         (each_openid,))
         each_openid_data = cursor_l.fetchall()
         need_delete_id = []
         keep_info = each_openid_data[0]
@@ -31,9 +28,7 @@ if __name__ == '__main__':
                 keep_info = each_gps
         if need_delete_id:
             counter_all_delete += len(need_delete_id)
-            cursor_l.execute(
-                "DELETE FROM USER_LOCATION WHERE ID in %s", (tuple(need_delete_id),)
-            )
+            cursor_l.execute("DELETE FROM USER_LOCATION WHERE ID in %s", (tuple(need_delete_id),))
             conn_l.commit()
         print u'清理进度：{}/{}, openid={}, 处理数据量={}'.format(counter, count_all, each_openid, len(each_openid_data))
 
